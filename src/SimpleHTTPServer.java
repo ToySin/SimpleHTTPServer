e84da95e33681e69c61ea445afb100c7f944ff7e
Toysin <toysin1203@hotmail.com>
2022-04-17 13:30:00 +0900

    현재 시간 응답하기

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class SimpleHTTPServer {
 
@@ -10,17 +12,23 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            Socket clientSocket = server.accept();
             // 1. Read HTTP request from the client socket.
             // 2. Prepare an HTTP response.
             // 3. Send HTTP response to the client.
             // 4. Close the socket.
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader br = new BufferedReader(isr);
-            String line = br.readLine();
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = br.readLine();
+
+            try (Socket clientSocket = server.accept()) {
+
+                InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+                BufferedReader br = new BufferedReader(isr);
+
+                String line;
+                while (!(line = br.readLine()).isEmpty())
+                    System.out.println(line);
+
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                clientSocket.getOutputStream().write(httpResponse.getBytes(StandardCharsets.UTF_8));
             }
         }
     }
